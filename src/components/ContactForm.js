cb3266f4e704a1a8951e3253cf3a1d1e84041ad2
Aliadabi <Aliadabi3780@gmail.com>
2020-06-13 00:58:14 -0700

    Done

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -16,10 +16,10 @@ const ContactForm = () => {
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            id= "firstName"
             name="firstName"
-            placeholder="Edd"
-            ref={register({ required: true, maxLength: 3 })}
-          />
+            placeholder="Ali"
+            ref={register({ required: true, maxLength: 5 })}          />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
           )}
@@ -28,8 +28,9 @@ const ContactForm = () => {
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            id="lastName"
             name="lastName"
-            placeholder="Burke"
+            placeholder="Adabi"
             ref={register({ required: true })}
           />
           {errors.lastName && (
@@ -41,21 +42,21 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input name="email" id="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
-          <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+        <label htmlFor="message" >Message</label>
+          <textarea name="message" id="message" ref={register({ required: false })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input data-testid="test" type="submit" />
       </form>
     </div>
   );
